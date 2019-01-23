d6ee21ebd0de2736a30b684fc27fce12b8266992
Pranit <pranit@fierydevs.com>
2019-01-23 10:18:37 +0530

    JSON Parsing

---
+++ app/src/main/java/com/morepranit/volleydemo/BookModel.java
@@ -0,0 +1,64 @@
+package com.morepranit.volleydemo;
+
+import java.util.ArrayList;
+
+public class BookModel {
+    private String id, title, publisher, publishedDate, description, thumbnail;
+    private ArrayList<String> authors;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getPublishedDate() {
+        return publishedDate;
+    }
+
+    public void setPublishedDate(String publishedDate) {
+        this.publishedDate = publishedDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getThumbnail() {
+        return thumbnail;
+    }
+
+    public void setThumbnail(String thumbnail) {
+        this.thumbnail = thumbnail;
+    }
+
+    public ArrayList<String> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(ArrayList<String> authors) {
+        this.authors = authors;
+    }
+}
--- app/src/main/java/com/morepranit/volleydemo/MainActivity.java
+++ app/src/main/java/com/morepranit/volleydemo/MainActivity.java
@@ -12,14 +12,24 @@ import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.NetworkImageView;
 import com.android.volley.toolbox.StringRequest;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     TextView tvData;
     NetworkImageView imageView;
+
+    ArrayList<BookModel> bookModels;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bookModels = new ArrayList<>();
 
         tvData = findViewById(R.id.tv_data);
         imageView = findViewById(R.id.imageView);
@@ -38,7 +48,14 @@ public class MainActivity extends AppCompatActivity {
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
-                        tvData.setText(response);
+                        // Log.e("onResponse: ", response);
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            parseJSON(jsonObject);
+                            tvData.setText(bookModels.get(9).getTitle());
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
                     }
                 }, new Response.ErrorListener() {
             @Override
@@ -48,6 +65,41 @@ public class MainActivity extends AppCompatActivity {
         });
 
         AppController.getInstance().addToRequestQueue(request, "req_tag");
+    }
+
+    private void parseJSON(JSONObject jsonObject) throws JSONException {
+        JSONArray jsonArray = jsonObject.getJSONArray("items");
+        for (int i = 0; i < jsonArray.length(); i++) {
+            JSONObject bookObject = jsonArray.getJSONObject(i);
+
+            String id = bookObject.getString("id");
+            JSONObject volInfo = bookObject.getJSONObject("volumeInfo");
+            String title = volInfo.getString("title");
+
+            JSONArray authors = volInfo.getJSONArray("authors");
+            ArrayList<String> authorsList = new ArrayList<>();
+            for (int j = 0; j < authors.length(); j++) {
+                String author = authors.getString(j);
+                authorsList.add(author);
+            }
+
+            String publisher = volInfo.has("publisher") ? volInfo.getString("publisher") : "";
+            String publishedDate = volInfo.has("publishedDate") ? volInfo.getString("publishedDate") : "";
+            String desc = volInfo.has("description") ? volInfo.getString("description") : "";
+
+            String thumbnail = volInfo.getJSONObject("imageLinks").getString("thumbnail");
+
+            BookModel bookModel = new BookModel();
+            bookModel.setId(id);
+            bookModel.setDescription(desc);
+            bookModel.setPublisher(publisher);
+            bookModel.setPublishedDate(publishedDate);
+            bookModel.setThumbnail(thumbnail);
+            bookModel.setTitle(title);
+            bookModel.setAuthors(authorsList);
+
+            bookModels.add(bookModel);
+        }
     }
 
     @Override
